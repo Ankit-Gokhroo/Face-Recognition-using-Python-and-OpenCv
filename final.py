4a41b69f122bf3151e481322f38d4e5ae16721ff
Rajat Arya <36148932+rajat1478@users.noreply.github.com>
2019-10-03 03:07:37 +0530

    Update final.py

--- final.py
+++ final.py
@@ -27,6 +27,7 @@ def getprofile(id):
 while(True):
     ret, img = cam.read();
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     faces = facedetector.detectMultiScale(gray, 1.3, 5);
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
